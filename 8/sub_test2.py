d289d188d5735c8cdd2b0fae4955b1d43e18ef21
Franciszek Stachura <fbstachura@gmail.com>
2023-12-17 23:12:15 +0100

    add tests for 8

---
+++ 8/sub_test2.py
@@ -0,0 +1,35 @@
+import random
+import subprocess
+
+while True:
+    proc = subprocess.Popen("./z3", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    a = random.randint(0, 2**128)
+    b = random.randint(0, 2**128)
+    result = a + b
+
+    (stdout, stderr) = proc.communicate(
+        str(a).encode("ascii") + b"\n" +
+        str(b).encode("ascii") + b"\n"
+    )
+
+    print(a)
+    print(b)
+
+    if proc.returncode == 0:
+        [add_result, _] = stdout.split(b"\n")
+
+        actual = abs(a-b)
+        received = int(add_result)
+
+        if actual != received:
+            print("invalid result")
+            print(actual)
+            print(received)
+            exit(-1)
+
+    else:
+        print("error")
+        exit(-1)
+
+
